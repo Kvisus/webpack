ae8ace733dd0deb77d41993e7a47ee3eb5c91e37
Kvisus <eternal.wanderer.z@gmail.com>
2023-12-17 19:55:23 +0300

    webpack ts vanila

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { calc } from "./test";
+console.log("hello there!");
+
+console.log(calc(5, 1));
---
+++ webpack.config.ts
@@ -0,0 +1,51 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import webpack from "webpack";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+type Mode = "development" | "production";
+
+interface EnvVarialbes {
+  mode: Mode;
+  port: number;
+}
+
+export default (env: EnvVarialbes) => {
+  const isDev = env.mode === "development";
+
+  const config: webpack.Configuration = {
+    mode: env.mode ?? "development",
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+      isDev && new webpack.ProgressPlugin(),
+    ].filter(Boolean),
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    devtool: isDev && "inline-source-map",
+    devServer: isDev
+      ? {
+          port: env.port ?? 3000,
+          open: true,
+        }
+      : undefined,
+  };
+  return config;
+};
