e4fea6d769af9ea37908a651270fec14f1db405a
Kvisus <eternal.wanderer.z@gmail.com>
2023-12-19 01:55:08 +0300

    lazy, svg, alias, modules

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
-import "./App.scss";
+import classes from "./App.module.scss";
+import { Link, Outlet } from "react-router-dom";
+import avatarpng from "@/assets/avatar.png";
+import Cat from "@/assets/cat.svg";
 export const App = () => {
   const [count, setCount] = useState(0);
   const increment = () => {
@@ -7,11 +10,21 @@ export const App = () => {
   };
   return (
     <div>
+      <div>
+        <img width={100} height={100} src={avatarpng} alt="" />
+      </div>
+      <div>
+        <Cat style={{ color: "darkblue" }} width={150} height={150} />
+      </div>
+      <Link to={"/about"}>About</Link>
+      <br />
+      <Link to={"/shop"}>Shop</Link>
       <h1>Hello there!</h1>
       <p>Count: {count}</p>
-      <button onClick={increment}>
+      <button className={classes.button} onClick={increment}>
         <span>Increment</span>
       </button>
+      <Outlet />
     </div>
   );
 };
